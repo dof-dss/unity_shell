14ce81a61558533622da8d7c198ded52718621e8
OMAHM <hello@onemanandhismouse.com>
2022-08-09 19:39:50 +0100

    D8UN-1256 ~ Method for fetching and parsing multiple file types

--- src/UnityShellCommand.php
+++ src/UnityShellCommand.php
@@ -7,6 +7,7 @@ use DrupalFinder\DrupalFinder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
 
 #[AsCommand(
     name: 'unity:shell',
@@ -38,8 +39,26 @@ class UnityShellCommand extends Command {
         return $this->fs->exists($this->rootPath() . $file_path);
     }
 
+    /**
+     * Read and parse contents of multiple file types.
+     *
+     * @param $file_path
+     * @return array|false|mixed|string|null
+     */
     public function fileRead($file_path) {
-        
+        if (!$this->fileExists($file_path)) {
+            return null;
+        }
+
+        switch (pathinfo($file_path, PATHINFO_EXTENSION)) {
+            case 'yaml':
+            case 'yml':
+                return Yaml::parseFile($this->rootPath() . $file_path);
+            case '.env':
+                return parse_ini_file($this->rootPath() . $file_path);
+            default:
+                return file_get_contents($this->rootPath() . $file_path);
+        }
     }
 
     public function fileWrite($file_path) {
