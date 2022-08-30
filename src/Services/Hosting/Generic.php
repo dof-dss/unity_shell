f0ed43c4cb9edd7f01f12806ef5af717b609ec1b
OMAHM <hello@onemanandhismouse.com>
2022-08-30 19:40:49 +0100

    Generic service for universal site setup and config

--- src/Services/Hosting/Generic.php
+++ src/Services/Hosting/Generic.php
@@ -2,48 +2,55 @@
 
 namespace UnityShell\Services\Hosting;
 
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use UnityShell\Hosting;
 use UnityShell\HostingInterface;
-use UnityShell\Utils;
 
-class Lando extends Hosting implements HostingInterface {
+class Generic extends Hosting implements HostingInterface {
 
   public function build($io) {
     parent::build($io);
-    $data = [];
 
-    $data['name'] = Utils::createApplicationId($this->project()->name());
-
+    $io->text('Verifying setup for ' . count($this->project()->sites()) . ' site(s).');
     foreach ($this->project()->sites() as $site_id => $site) {
+      // If a site folder doesn't exist under project/sites, create it and
+      // provide a settings file.
+      if (!$this->fs()->exists('/project/sites/' . $site_id)) {
+        $io->text('Creating a site directory for ' . $site_id . ' under project/sites/');
+        $this->fs()->mkdir('/project/sites/' . $site_id);
+        $this->fs()->copy('/.hosting/Generic/resources/multisite.settings.php', '/project/sites/' . $site_id . '/settings.php');
+      }
 
-      // Create Lando proxy.
-      $data['proxy']['appserver'][] = $site['url'] . '.lndo.site';
+      // Enable our multisite entry by linking from the sites directory to
+      // the project directory.
+      try {
+        $this->fs()->symlink('//app/project/sites/' . $site_id, '/web/sites/' . $site_id);
+      }
+      catch (IOExceptionInterface $exception) {
+        $io->error("An error occurred while linking $site_id site directory: " . $exception->getMessage());
+      }
 
-      // Create solr relationship.
-      if (!empty($site['solr'])) {
-        $data['services'][$site_id . '_solr'] = [
-          'type' => 'solr:7',
-          'portforward' => TRUE,
-          'core' => 'default',
-          'config' => [
-            'dir' => '.lando/config/solr/7.x/default',
-          ],
-        ];
+      // If a site config doesn't exist under project/config, create it.
+      if (!$this->fs()->exists('/project/config/' . $site_id)) {
+        $io->text('Creating config directory for ' . $site_id . ' under project/config/');
+        $this->fs()->mkdir('/project/config/' . $site_id);
+        $this->fs()->dumpFile('/project/config/' . $site_id . '/.gitkeep', "");
+
+        // Create the default config directories if they don't already exist.
+        foreach (['config', 'hosted', 'local', 'production'] as $directory) {
+          $io->text('Creating default config directories');
+          if (!$this->fs()->exists('/project/config/' . $site_id . '/' . $directory)) {
+            $this->fs()->mkdir('/project/config/' . $site_id . '/' . $directory);
+          }
+        }
       }
     }
 
-    // Copy the base Unity configuration for Lando.
-    $io->writeln("Creating Lando base configuration file.");
-    $this->fs()->copy('/.hosting/Lando/templates/.lando.base.yml', '/.lando.base.yml');
-
-    // Create project specific Lando file.
-    $io->writeln("Creating Lando project configuration file.");
-    $this->fs()->dumpFile('/.lando.yml', $data);
-
-    // Copy Lando resources to the project.
-    $io->writeln("Copying Lando resources to project.");
-    $this->fs()->mkdir('/.lando');
-    $this->fs()->mirror('/.hosting/Lando/resources/', '/.lando');
+    // Copy base Drupal services file is one doesn't already exist.
+    if (!$this->fs()->exists('/web/sites/services.yml')) {
+      $io->text('Creating Drupal services file from defaults.');
+      $this->fs()->copy('/.hosting/Generic/resources/default.services.yml', '/web/sites/services.yml');
+    }
   }
 
 }
